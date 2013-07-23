1e2e06b2891d7d16d8497ae124776ee6396c2dde
Christopher Petersen <chris@petersen.io>
2013-07-22 20:50:54 -0700

    Working on icons

--- resources/create-icons.sh
+++ resources/create-icons.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-convert iTunesArtwork@2x.png -resize "1024x" iTunesArtwork.png
+convert iTunesArtwork@2x.png -resize "512x" iTunesArtwork.png
 convert iTunesArtwork@2x.png -resize "57x" Icon.png
 convert iTunesArtwork@2x.png -resize "72x" Icon-72.png
 convert iTunesArtwork@2x.png -resize "114x" Icon@2x.png
